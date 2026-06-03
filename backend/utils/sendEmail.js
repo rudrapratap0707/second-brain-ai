fef50f28f9bc1e5f4bf7e1ac3bc0de36c581ade5
Rudra Pratap Tripathi <rudrapratappandey2020@gmail.com>
2026-06-03 14:33:43 +0530

    Switch forgot password emails to Resend

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,22 +1,18 @@
-const nodemailer = require("nodemailer")
+const { Resend } = require("resend")
 
-const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  })
+const resend = new Resend(
+  process.env.RESEND_API_KEY
+)
 
-  await transporter.sendMail({
-    from: `"Second Brain AI" <${process.env.EMAIL_USER}>`,
+const sendEmail = async ({
+  to,
+  subject,
+  html,
+}) => {
+  await resend.emails.send({
+    from:
+      "Second Brain AI <onboarding@resend.dev>",
+
     to,
     subject,
     html,
