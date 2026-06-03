61e83dadcb50646f458ce90b54fb2aa5c6da4b2c
Rudra Pratap Tripathi <rudrapratappandey2020@gmail.com>
2026-06-03 14:20:09 +0530

    Add email timeout

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,6 +6,9 @@ const sendEmail = async ({ to, subject, html }) => {
     port: 587,
     secure: false,
     family: 4,
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
